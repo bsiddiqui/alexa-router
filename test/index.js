5fd1fd80718a1199a4b222bc8adf85f54f771b10
Alan Hoffmeister <alanhoffmeister@gmail.com>
2016-07-06 23:30:18 -0300

    First usable code

---
+++ test/index.js
@@ -0,0 +1,81 @@
+'use strict'
+
+let assert = require('assert')
+
+let AlexaFramework = require('../')
+let alexa = new AlexaFramework()
+
+let examples = {
+  sayHello: {
+    version: '1.0',
+    session: {
+      new: true,
+      sessionId: 'this-should-be-an-id',
+      application: {
+        applicationId: 'weird-id-wuh?'
+      },
+      attributes: {},
+      user: {
+        userId: 'this-is-an-id'
+      }
+    },
+    request: {
+      type: 'IntentRequest',
+      requestId: 'another-id',
+      timestamp: new Date().toJSON(),
+      intent: {
+        name: 'say-hello',
+        slots: {}
+      }
+    }
+  },
+  ok: {
+    version: '1.0',
+    session: {
+      new: true,
+      sessionId: 'this-should-be-an-id',
+      application: {
+        applicationId: 'weird-id-wuh?'
+      },
+      attributes: {
+        referrer: '/say-hello'
+      },
+      user: {
+        userId: 'this-is-an-id'
+      }
+    },
+    request: {
+      type: 'IntentRequest',
+      requestId: 'another-id',
+      timestamp: new Date().toJSON(),
+      intent: {
+        name: 'ok',
+        slots: {}
+      }
+    }
+  }
+}
+
+alexa.route('/say-hello', (request, response) => {
+  response.speech('<say>Hello world!</say>')
+})
+
+alexa.route('/say-hello/ok', (request, response) => {
+  response.speech('Ok then, it worked')
+  response.endSession(true)
+})
+
+alexa.dispatch(examples.sayHello)
+.then((res) => {
+  let json = res.toJSON()
+  assert.equal(json.response.outputSpeach.type, 'SSML')
+  assert.equal(json.response.outputSpeach.ssml, '<say>Hello world!</say>')
+
+  return alexa.dispatch(examples.ok)
+})
+.then((res) => {
+  let json = res.toJSON()
+  assert.equal(json.response.outputSpeach.type, 'PlainText')
+  assert.equal(json.response.outputSpeach.ssml, 'Ok then, it worked')
+  assert.equal(json.response.shouldEndSession, true)
+})
